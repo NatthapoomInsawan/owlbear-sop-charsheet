5e26ebb33c3c1ed5eea30abe3e11be8d4f31930e
Natthapoom Insawan <natthapoom654@outlook.com>
2026-03-20 05:43:01 +0700

    update : added character,css and swap betwen player and character input field

--- src/views/character.js
+++ src/views/character.js
@@ -1,3 +1,5 @@
+import "../CSS/character.css";
+
 import AbstractView from "./AbstractView";
 
 export default class Character extends AbstractView{
@@ -10,8 +12,8 @@ export default class Character extends AbstractView{
         return /*html*/`
         <h1>Character</h1>
         <div class = "character-info">
-            <label>PLAYER <input placeholder = "name" data-character = "playerName"></label>
             <label>CHARACTER <input placeholder = "name" data-character = "characterName"></label>
+            <label>PLAYER <input placeholder = "name" data-character = "playerName"></label>
             <label>CLASS <input placeholder = "class" data-character = "characterClass" list = "character-class"></label>
             <label>SUBCLASS <input placeholder = "subclass" data-character = "characterSubclass" list = "character-subclass"></label>
             <label>LINEAGE <input placeholder = "lineage" data-character = "characterLineage" list = "character-lineage"></label>
